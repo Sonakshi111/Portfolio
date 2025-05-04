f08c0dd7d991c4271179c5674b82bbedeb5f3e2a
Sonakshi <sonakshiy05@gmail.com>
2025-05-04 15:59:17 +0530

    update

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -13,7 +13,7 @@ const portfolioProjects = [
       { title: "Built interactive UI with Streamlit" },
       { title: "Real-time results based on user input" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "https://github.com/Sonakshi111/Heart-Disease-Prediction.git",
     image: HeartDiseasePred,
   },
   {
@@ -23,7 +23,7 @@ const portfolioProjects = [
       { title: "Responsive layout with sliding image feature" },
       { title: "Designed for visual appeal and interactivity" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: "https://github.com/Sonakshi111/Anime-website.git",
     image: AnimeWebiste,
   },
 
@@ -44,42 +44,47 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">Real World Results</p>
         </div>
 
-        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
-        <p className="text-center text-white/60 mt-4">See how i transformed concepts into digital experiences</p>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center md:text-xl lg:text-2xl text-white/60 mt-4 max-w-md mx-auto">See how I transformed concepts into digital experiences</p>
 
-        <div className="flex flex-col mt-10 gap-20">
+        <div className=" mt-10 md:mt-20 lg:mt-30 flex flex-col gap-20">
           {portfolioProjects.map(project => (
-            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8">
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none">
 
-              <div>
-                <div className="flex">
-                  <h3 className="font-serif text-2xl mt-2 font-semibold tracking-widest bg-gradient-to-r from-blue-300 to-red-400 text-transparent bg-clip-text">{project.title}</h3>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="flex">
+                    <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl lg:text-5xl font-semibold tracking-widest bg-gradient-to-r from-blue-300 to-red-400 text-transparent bg-clip-text">{project.title}</h3>
+                  </div>
+
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5 lg:mt-10" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5 lg:mt-8">
+                    {project.results.map((result) => (
+                      <li className="flex gap-2 text-sm md:text-base lg:text-xl text-white/50">
+                        <CheckIcon className='size-5 md:size-6' />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <a href={project.link} target="_blank">
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-bold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>Visit The Project</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
                 </div>
-
-                <hr className="border-t-2 border-white/5 mt-4" />
-                <ul className="flex flex-col gap-4 mt-4">
-                  {project.results.map((result) => (
-                    <li className="flex gap-2 text-sm text-white/50">
-                      <CheckIcon className='size-5' />
-                      <span>{result.title}</span>
-                    </li>
-                  ))}
-                </ul>
-
-                <a href={project.link}>
-                  <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-bold inline-flex items-center justify-center gap-2 mt-8">
-                    <span>Visit The Project</span>
-                    <ArrowUpRightIcon className="size-4" />
-                  </button>
-                </a>
-                <Image src={project.image} alt={project.title} className="mt-8" />
+                <div>
+                  <Image src={project.image} alt={project.title} className="mt-8 lg:mt-0 lg:absolute lg:h-fit lg:w-fit" />
+                </div>
               </div>
+
             </div>
           ))}
 
