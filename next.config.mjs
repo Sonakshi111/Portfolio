494e9659dbb4137791a939bf1c75326e6a4fb4aa
Sonakshi <sonakshiy05@gmail.com>
2025-05-07 02:32:27 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
@@ -44,8 +45,6 @@ const nextConfig = {
     return config;
   },
 
-  //Enable static export
-  output:'export',
 };
 
 export default nextConfig;
